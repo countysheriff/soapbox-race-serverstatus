592af32dc401c2aa24ae9c440e650177bc009222
Nilzao <nilzao@gmail.com>
2017-07-05 20:35:45 -0400

    cache example

--- src/ServerList.php
+++ src/ServerList.php
@@ -4,20 +4,56 @@
 class ServerList
 {
 
+    private static $tmpFilePath = "tmp/list.txt";
+
+    private static $serverListSize = 0;
+
     /**
      *
      * @return ServerObj[]
      */
     public static function getServerObjectList()
     {
-        $serverList = file_get_contents("tmp/list.txt");
-        $serverListSize = 3;
-        ;
+        $fileTime = filemtime(self::$tmpFilePath) + 60;
+        $date = new DateTime();
+        $actualTime = $date->getTimestamp();
+        if ($actualTime > $fileTime) {
+            $data = file_get_contents("https://raw.githubusercontent.com/nilzao/soapbox-race-hill/master/serverlist-v2.txt");
+            file_put_contents(self::$tmpFilePath, $data);
+            self::writeJson();
+        } else {
+            self::countServerListSize();
+        }
         $srvArrLst = array();
-        for ($i = 1; $i <= $serverListSize; $i ++) {
-            $serverTest = file_get_contents("tmp/00{$i}.json");
+        for ($i = 0; $i < self::$serverListSize; $i ++) {
+            $serverTest = file_get_contents("tmp/{$i}.json");
             $srvArrLst[] = new ServerObj("test" . $i, "http://localhost:8680", $serverTest);
         }
         return $srvArrLst;
+    }
+
+    private static function countServerListSize()
+    {
+        $handle = fopen(self::$tmpFilePath, "r");
+        while (($line = fgets($handle)) !== false) {
+            self::$serverListSize ++;
+        }
+        fclose($handle);
+    }
+
+    private static function writeJson()
+    {
+        self::$serverListSize = 0;
+        $handle = fopen(self::$tmpFilePath, "r");
+        if ($handle) {
+            while (($line = fgets($handle)) !== false) {
+                $serverInfo = explode(";", $line);
+                $serverUrl = trim($serverInfo[1]) . "/soapbox-race-core/Engine.svc/GetServerInformation";
+                $jsonInfo = file_get_contents($serverUrl);
+                file_put_contents("tmp/" . self::$serverListSize . ".json", $jsonInfo);
+                self::$serverListSize ++;
+            }
+            fclose($handle);
+        }
     }
 }
